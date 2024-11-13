ad2eb381e150d0947563609e2bec980c647eb90f
danny160 <125933927+danny160@users.noreply.github.com>
2024-11-13 11:29:04 -0500

    actualizacion 3

--- paginas/categorias.php
+++ paginas/categorias.php
@@ -109,9 +109,9 @@
 
         <!-- Contenedor principal -->
         <div class="content">
-
-        
+            
         </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
