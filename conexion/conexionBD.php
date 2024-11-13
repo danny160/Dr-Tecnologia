151cf9afe11fcd549ed07f67d6070d3905abd0f6
danny160 <125933927+danny160@users.noreply.github.com>
2024-11-13 10:59:01 -0500

    Update conexionBD.php

--- conexion/conexionBD.php
+++ conexion/conexionBD.php
@@ -1,6 +1,6 @@
 <?php
 // Datos de la conexión
-$host = "localhost";  // Servidor de la base de datos (localhost si usas XAMPP)
+$host = "localho";  // Servidor de la base de datos (localhost si usas XAMPP)
 $username = "root";    // Usuario de MySQL (por defecto es 'root' en XAMPP)
 $password = "";        // Contraseña del usuario MySQL (por defecto está vacía en XAMPP)
 $database = "drtecnologia";  // Nombre de tu base de datos
